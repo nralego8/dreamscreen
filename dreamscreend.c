ac475e236375ff0ec7b31ce57572294fbc76da56
nralego8 <nralego8@gmail.com>
2020-05-03 14:23:37 -0700

    formatting

--- dreamscreend.c
+++ dreamscreend.c
@@ -17,42 +17,6 @@ int red = -1;
 int green;
 int blue;
 
-
-#include <stdio.h>
-#include <math.h>
-
-void rgb_to_xy(double red, double green, double blue) {
-	double normalized[3];
-	
-	normalized[0] = red/255;
-	normalized[1] = green/255;
-	normalized[2] = blue/255;
-	
-	if (normalized[0] > 0.04045)
-		red = (double)pow((normalized[0] + 0.055) / (1.0 + 0.055), 2.4);
-	else
-		red = (double)(normalized[0] / 12.92);
-	
-	if (normalized[1] > 0.04045)
-		green = (double)pow((normalized[1] + 0.055) / (1.0 + 0.005), 2.4);
-	else
-		green = (double)(normalized[1] / 12.92);
-
-	if (normalized[2] > 0.04045)
-		blue = (double)pow((normalized[2] + 0.055) / (1.0 + 0.005), 2.4);
-	else
-		blue = (double)(normalized[2] / 12.92);
-
-	double X = (double) (red * 0.649926 + green * 0.103455 + blue * 0.197109);
-	double Y = (double) (red * 0.234327 + green * 0.743075 + blue * 0.022598);
-	double Z = (double) (red * 0.000000 + green * 0.053077 + blue * 1.035763);
-
-	double x = X / (X + Y + Z);
-	double y = Y / (X + Y + Z);
-}	
-
-	
-
 char *rgb_convert(double r, double g, double b) {
 	r = r/255;
 	g = g/255;
@@ -202,8 +166,6 @@ void assemble_packet_rgb(unsigned char packet[], unsigned char prefix[], unsigne
 	packet[9] = calcCRC8(packet);
 }
 
-
-
 int dream() {
 	int i, sockfd, p;
 	struct sockaddr_in serveraddr;
@@ -218,7 +180,6 @@ int dream() {
 	int members = 3;
 	int portno = 8888;
 
-	/* hex codes used by Dreamscreen: https://planet.neeo.com/media/80x1kj/download/dreamscreen-v2-wifi-udp-protocol.pdf */
 	unsigned char prefix[] = { 0xFC, 0x06, 0x01, 0x11, 0x03 };
 	unsigned char special[] = { 0xFC, 0x05, 0xFF, 0x30, 0x01 };
 
@@ -321,8 +282,6 @@ int dream() {
 		exit(EXIT_FAILURE);
 	}
 
-	
-	assemble_packet_rgb(packet_rgb, prefix, red, green, blue);
 	/* build Dreamscreen address */
 	bzero((char *) &serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
